0d973222b5f70d6ab367617794744db461fbfc5a
DennisDaddy <softteez95@gmail.com>
2016-07-07 19:48:10 +0300

    fill the layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
 
-  get 'pages/about'
+  get  'about' => 'pages#about'
 
-  get 'pages/admin'
+  get 'admin' => 'pages#admin'
 
-  get 'pages/contact'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'contact' => 'pages#contact'
 end
