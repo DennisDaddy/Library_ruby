54df31dc19e1cd335b8bbfb31466273f21043683
DennisDaddy <softteez95@gmail.com>
2016-07-10 15:17:21 +0300

    create login form

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,11 @@ Rails.application.routes.draw do
 
   get 'signup' => 'users#new'
 
+  get 'login' => 'sessions#new'
+
+  post 'login' => 'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
+
   resources :users
 end
