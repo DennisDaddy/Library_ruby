d24e031566a771c1e7cda7638b91ce5f6d293107
DennisDaddy <softteez95@gmail.com>
2016-07-19 10:25:34 +0300

    generate controller PasswordReset and fill password reset form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'users/new'
+ 
 
   root 'pages#home'
 
@@ -24,4 +22,5 @@ Rails.application.routes.draw do
   resources :users
 
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
