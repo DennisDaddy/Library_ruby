8a57a3a9ce9f338c5bf79b49acbfa701d89b8e17
DennisDaddy <softteez95@gmail.com>
2016-07-22 01:41:57 +0300

    create admin

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,6 +21,9 @@ def current_user
 end
 end
 end
+def current_user?(user)
+  user == current_user
+end
 
 def forget(user)
 	user.forget
@@ -36,5 +39,12 @@ def log_out
 	forget(current_user)
 	session.delete(:user_id)
 	@current_user = nil
+end
+def redirect_back_or(default)
+  redirect_to(session[:forwarding_url] || default)
+  session.delete(:forwarding_url)
+end
+def store_location
+  session[:forwarding_url] = request.url if request.get?
 end
 end
